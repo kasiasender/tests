8a7cc0e65fcd5c6b18a9e0f13eb1a4d5bcfc343a
kasiasender <kasiasender138@gmail.com>
2016-04-10 12:01:43 +0200

    Update base.py

--- base.py
+++ base.py
@@ -6,7 +6,6 @@ class Page(object):
     def __init__(self, driver, base_url="http://demo.opencart.com/"):
         self.base_url = base_url
         self.driver = driver
-        self.timeout = 30
 
     def find_element(self, *locator):
         return self.driver.find_element(*locator)
